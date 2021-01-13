e9dea5194f0f6c35e31ddde2ceae9bf12575ff7b
Zeeshan Khan <khanmzeeshan@gmail.com>
2021-01-13 10:10:58 -0800

    All requirements and POST complete

--- sample_backend.py
+++ sample_backend.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 app = Flask(__name__)
+
+#@app.route('/users', methods=['GET', 'POST'])
 
 @app.route('/')
 def hello_world():
@@ -33,20 +36,15 @@ users = {
          'id' : 'zap555', 
          'name': 'Dennis',
          'job': 'Bartender',
+      },
+      {
+        "id": "qwe123",
+        "job": "Zookeeper",
+        "name": "Cindy"
       }
    ]
 }
 
-@app.route('/users')
-def get_users():
-   search_username = request.args.get('name') # accessing the value of parameter 'name'
-   if search_username:
-      subdict = {'users_list' : []}
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
 
 @app.route('/users/<id>')
 def get_user(id):
@@ -56,3 +54,23 @@ def get_user(id):
            return user
       return ({})
    return users
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')  # accessing the value of parameter 'name'
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      fetch("http://127.0.0.1:5000/users")
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
